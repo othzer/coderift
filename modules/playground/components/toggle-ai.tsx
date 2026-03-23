047fe9302914192c386412509887b9cc431cb0ba
Stanzin Othzer <228166799+othzer@users.noreply.github.com>
2026-03-23 21:20:40 +0530

    12 AI Module part 2: Implemented the chat with ai feature

--- modules/playground/components/toggle-ai.tsx
+++ modules/playground/components/toggle-ai.tsx
@@ -26,6 +26,7 @@ import {
 } from "lucide-react";
 import React from "react";
 import { cn } from "@/lib/utils";
+import { AIChatSidePanel } from "@/modules/ai-chat/components/ai-chat-sidebarPanel";
 // import { AIChatSidePanel } from "@/modules/ai-chat/components/ai-chat-sidebarpanel";
 
 
@@ -167,10 +168,10 @@ const ToggleAI: React.FC<ToggleAIProps> = ({
         </DropdownMenuContent>
       </DropdownMenu>
 
-        {/* <AIChatSidePanel
+      <AIChatSidePanel
         isOpen={isChatOpen}
         onClose={() => setIsChatOpen(false)}
-        /> */}
+      />
     </>
   );
 };
